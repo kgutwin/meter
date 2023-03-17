d28343ffe110c5ab33d216e00357bec766c99dbe
Karl Gutwin <karl@bioteam.net>
2023-03-17 09:29:51 -0400

    Improve weather polling

--- meter/sources/weather.py
+++ meter/sources/weather.py
@@ -1,6 +1,6 @@
 import json
-import time
 from urllib.request import urlopen
+from datetime import datetime, timedelta, timezone
 
 from meter.utils import c_to_f
 from meter.sources.base import BaseSource
@@ -13,13 +13,27 @@ class OutsideTemp(BaseSource):
         (['--coords'], {"required": True}),
     ]
 
+    def init(self):
+        self.last_update = datetime.min
+    
     def update(self, reported):
-        time.sleep(15)
-        lat, lon = self.opts.coords.split(',')
-        url = (f'https://api.open-meteo.com/v1/forecast?'
-               f'latitude={lat}&longitude={lon}&current_weather=true')
-        response = json.load(urlopen(url))
-        self.log(response)
-        return {
-            'meter': c_to_f(response['current_weather']['temperature'])
-        }
+        if datetime.now() > self.last_update + timedelta(hours=1):
+            lat, lon = self.opts.coords.split(',')
+            url = (f'https://api.open-meteo.com/v1/forecast?'
+                   f'latitude={lat}&longitude={lon}&current_weather=true')
+            response = json.load(urlopen(url))
+            
+            # convert the timestamp in the response to our current
+            # timezone as a naive datetime object
+            self.last_update = datetime.strptime(
+                response['current_weather']['time'],
+                '%Y-%m-%dT%H:%M'
+            ).replace(tzinfo=timezone.utc).astimezone().replace(tzinfo=None)
+            
+            self.log(response)
+            return {
+                'meter': c_to_f(response['current_weather']['temperature'])
+            }
+        
+        else:
+            return {}
